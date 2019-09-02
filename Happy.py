0f33737f4f3c2b24cedd204f95648d23d1e14c98
Akash Mullick <50779163+AkashMullick@users.noreply.github.com>
2019-09-01 20:28:10 -0400

    Create Happy.py

---
+++ Happy.py
@@ -0,0 +1,3 @@
+# Instructions: Write a piece of Python code that prints out the string 'hello world' if the value of an integer variable, happy, is strictly greater than 2.
+if happy > 2:
+  print("hello world")

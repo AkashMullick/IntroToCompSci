b97f5f1cf107e0dc47af0702954752eb7d919b13
Akash Mullick <akashmullick1@gmail.com>
2020-02-02 14:06:41 -0500

    Calculate the square of a number

---
+++ Square.py
@@ -0,0 +1,11 @@
+'''Instructions: Write a Python function, square, that takes in one number and returns the square of that number.
+This function takes in one number and returns one number.'''
+
+def square(x):
+    '''
+    Input: x, int or float.
+    Returns the square of x
+    '''
+    return x**2
+
+print(square(int(input("Square of: "))))
